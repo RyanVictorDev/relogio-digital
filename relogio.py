95e34065ac55f788bf7b32ffaf70f7cdddd0b1c7
RyanVictorDev <vryan8294@gmail.com>
2023-07-04 15:05:15 -0300

    atualização0104072023

---
+++ relogio.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+import datetime
+
+def hora_atual():
+    current_time = datetime.datetime.now().strftime("%H:%M:%S")
+    time_label.config(text=current_time)
+    time_label.after(1000, hora_atual)
+
+# Cria uma janela
+window = tk.Tk()
+window.title("Relógio Digital")
+
+# Cria um rótulo para exibir a hora
+time_label = tk.Label(window, font=("Arial", 80))
+time_label.pack(padx=50, pady=50)
+
+# Inicia a atualização da hora
+hora_atual()
+
+# Inicia o loop principal da janela
+window.mainloop()
